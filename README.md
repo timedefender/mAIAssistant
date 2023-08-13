a31d7f6e5227b0f8b933a5f1a92b1256bb2fed63
Sergey Sokolyuk <sv.sokoluk@gmail.com>
2023-08-13 15:36:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# This project is inspired by and uses parts of code from
+https://github.com/daveshap/ChromaDB_Chatbot_Public
+
+# TODO
+
+- Search internet <input type="checkbox" checked disabled />
+- Conversation memory <input type="checkbox" checked disabled />
+- Ingest files 
+- Ingest directories 
+- Ingest video
+- Ingest audio/podcasts
+- Ingest websites
+- ability for offline model to ask questions to online big model like gpt-4 if it does not know the answer
+- Text to speech and speech to text interface
+- ability to call different expert models for different topics - swarm mode
+- ability to spawn autonomous agents 
+- Ability to browse the web (maybe headless browser+selenium)
+- Web interface
+- Client-server architecture to run in the cloud (remember to think about database synchronization and backup)
+- ability to use bing chat and google bard
+- Personas. Different LLMs trained to behave like coaches, scientists, businessmen. Train open source models on publícally available content of top performers and make the base assistant interact with them to provide you necessary information.
+- Ability to execute code in docker container 
+- Multiple languages of communication
---
+++ main.py
@@ -0,0 +1,172 @@
+import openai
+from langchain import SerpAPIWrapper
+from langchain.agents import initialize_agent, Tool, AgentType
+from langchain.chat_models import ChatOpenAI
+import langchain
+import chromadb
+from chromadb.config import Settings
+from time import time
+import os
+from uuid import uuid4
+import re
+
+config = {}
+langchain.debug = False
+openai_api_max_retries = 7
+
+def read_config(file_path):
+    result_dict = {}
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()  # Remove leading/trailing whitespaces and newline characters
+            if line:
+                key, value = line.split('=')
+                result_dict[key] = value
+    
+    return result_dict
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8', errors='ignore') as infile:
+        return infile.read()
+
+def save_file(filepath, content):
+    directory = os.path.dirname(filepath)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
+
+def chatbot(messages):
+    llm = ChatOpenAI(temperature=0, model=config['OPENAI_MODEL'], openai_api_key=config['OPENAI_API_KEY'])
+    search = SerpAPIWrapper(serpapi_api_key=config['SERPAPI_API_KEY'])
+    
+    tools = [
+    Tool(
+        name="Search",
+        func=search.run,
+        description="Useful when you need to answer questions about current events. You should ask targeted questions.",
+        ),
+    ]
+
+    mrkl = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+
+    return mrkl.run(messages)
+
+if __name__ == '__main__':
+    config = read_config('.env')
+
+    conversation = list()
+    conversation.append({'role': 'system', 'content': open_file('prompts/system_default.txt')})
+    user_messages = list()
+    all_messages = list()
+
+    chroma_client = chromadb.PersistentClient(path=config['CHROMADB_PERSIST_DIRECTORY'], settings=Settings(anonymized_telemetry=False))
+    collection = chroma_client.get_or_create_collection(name="knowledge_base")
+
+    # backup user profile
+    save_file('data/user_profile_backup.txt', open_file('data/user_profile.txt'))
+
+    while True:
+        # get user input
+        text = input('\n\nUSER: ')
+        user_messages.append(text)
+        all_messages.append('USER: %s' % text)
+        conversation.append({'role': 'user', 'content': text})
+        save_file('data/chat_logs/chat_%s_user.txt' % time(), text)
+
+
+        # update main scratchpad
+        if len(all_messages) > 5:
+            all_messages.pop(0)
+        main_scratchpad = '\n\n'.join(all_messages).strip()
+
+
+        # search KB, update default system
+        current_profile = open_file('data/user_profile.txt')
+        kb = 'No KB articles yet'
+        if collection.count() > 0:
+            results = collection.query(query_texts=[main_scratchpad], n_results=1)
+            kb = results['documents'][0][0]
+            #print('\n\nDEBUG: Found results %s' % results)
+        default_system = open_file('prompts/system_default.txt').replace('<<PROFILE>>', current_profile).replace('<<KB>>', kb)
+        #print('SYSTEM: %s' % default_system)
+        conversation[0]['content'] = default_system
+
+
+        # generate a response
+        response = chatbot(conversation)
+        save_file('data/chat_logs/chat_%s_chatbot.txt' % time(), response)
+        conversation.append({'role': 'assistant', 'content': response})
+        all_messages.append('CHATBOT: %s' % response)
+        print('\n\nCHATBOT: %s' % response)
+
+
+        # update user scratchpad
+        if len(user_messages) > 3:
+            user_messages.pop(0)
+        user_scratchpad = '\n'.join(user_messages).strip()
+
+
+        # update user profile
+        print('\n\nUpdating user profile...')
+        profile_length = len(current_profile.split(' '))
+        profile_conversation = list()
+        profile_conversation.append({'role': 'system', 'content': open_file('prompts/system_update_user_profile.txt').replace('<<UPD>>', current_profile).replace('<<WORDS>>', str(profile_length))})
+        profile_conversation.append({'role': 'user', 'content': user_scratchpad})
+        dirty_profile = chatbot(profile_conversation)
+        pattern = r"\[{'role': 'assistant', 'content': '([^']+)'}\]"
+        match = re.search(pattern, dirty_profile)
+        profile = match.group(1)
+        save_file('data/user_profile.txt', profile)
+
+
+        # update main scratchpad
+        if len(all_messages) > 5:
+            all_messages.pop(0)
+        main_scratchpad = '\n\n'.join(all_messages).strip()
+
+
+        # Update the knowledge base
+        print('\n\nUpdating KB...')
+        if collection.count() == 0:
+            # yay first KB!
+            kb_convo = list()
+            kb_convo.append({'role': 'system', 'content': open_file('prompts/system_instantiate_new_kb.txt')})
+            kb_convo.append({'role': 'user', 'content': main_scratchpad})
+            article = chatbot(kb_convo)
+            new_id = str(uuid4())
+            collection.add(documents=[article],ids=[new_id])
+            save_file('data/db_logs/log_%s_add.txt' % time(), 'Added document %s:\n%s' % (new_id, article))
+        else:
+            results = collection.query(query_texts=[main_scratchpad], n_results=1)
+            kb = results['documents'][0][0]
+            kb_id = results['ids'][0][0]
+            
+            # Expand current KB
+            kb_convo = list()
+            kb_convo.append({'role': 'system', 'content': open_file('prompts/system_update_existing_kb.txt').replace('<<KB>>', kb)})
+            kb_convo.append({'role': 'user', 'content': main_scratchpad})
+            article = chatbot(kb_convo)
+            collection.update(ids=[kb_id],documents=[article])
+            save_file('data/db_logs/log_%s_update.txt' % time(), 'Updated document %s:\n%s' % (kb_id, article))
+            # TODO - save more info in DB logs, probably as YAML file (original article, new info, final article)
+            
+            # Split KB if too large
+            kb_len = len(article.split(' '))
+            if kb_len > 1000:
+                kb_convo = list()
+                kb_convo.append({'role': 'system', 'content': open_file('prompts/system_split_kb.txt')})
+                kb_convo.append({'role': 'user', 'content': article})
+                articles = chatbot(kb_convo).split('ARTICLE 2:')
+                a1 = articles[0].replace('ARTICLE 1:', '').strip()
+                a2 = articles[1].strip()
+                collection.update(ids=[kb_id],documents=[a1])
+                new_id = str(uuid4())
+                collection.add(documents=[a2],ids=[new_id])
+                save_file('data/db_logs/log_%s_split.txt' % time(), 'Split document %s, added %s:\n%s\n\n%s' % (kb_id, new_id, a1, a2))
+
+            # re-print last response so it is not lost among langchain verbose output
+            print('\n\nCHATBOT: %s' % response)
